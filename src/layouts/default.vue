985d036d7ba930418ee0299cc031a5fef902e25b
Antohan <justanton19@gmail.com>
2020-01-25 12:58:58 +0700

    Improve default layout markup. Improve header. Add logo and buttons.

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,24 +1,36 @@
 <template>
   <section>
-    <header>
-      <router-link
-        :to="{ name: 'home' }"
-        :active-class="$style.activeLink"
-        exact
+    <header :class="$style.header">
+      <img
+        :class="$style.logo"
+        src="@/assets/logo.png"
+        alt="logo"
       >
-        Home
-      </router-link>
 
-      <router-link
-        :to="{ name: 'about' }"
-        :active-class="$style.activeLink"
-        exact
-      >
-        About
-      </router-link>
+      <nav :class="$style.nav">
+        <router-link
+          :to="{ name: 'home' }"
+          :class="$style.navigateLink"
+          :active-class="$style.activeLink"
+          exact
+        >
+          Home
+        </router-link>
+
+        <router-link
+          :to="{ name: 'about' }"
+          :class="$style.navigateLink"
+          :active-class="$style.activeLink"
+          exact
+        >
+          About
+        </router-link>
+      </nav>
+
+      <switch-button v-model="isAuth" />
     </header>
 
-    <div>
+    <div :class="$style.pageContent">
       <router-view/>
     </div>
   </section>
@@ -27,11 +39,45 @@
 <script>
 export default {
   name: 'DefaultLayout',
+  data: () => ({
+    isAuth: false,
+  }),
 };
 </script>
 
 <style module>
+.header {
+  padding: 0 20px;
+  box-shadow: 0 0 10px var(--shadow-color);
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.logo {
+  height: 70%;
+  transform: rotate(-90deg);
+}
+
+.nav {
+  width: 100%;
+  max-width: 960px;
+  margin: 20px;
+
+}
+
+.navigateLink:not(:last-child) {
+  margin-right: 20px;
+}
+
 .activeLink {
   color: var(--primary-color);
+}
+
+.pageContent {
+  width: 100%;
+  max-width: 1024px;
+  margin: auto;
 }
 </style>
